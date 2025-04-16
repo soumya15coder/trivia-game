439302f450a774fdecb3ee1e746f9131266fb3bc
soumya15coder <jaiswal788222@sas.edu.sg>
2025-04-16 14:09:38 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function startGame() {
+  window.location.href = "game.html"; // you'll create this next
+}
+
+function goToHighScores() {
+  window.location.href = "highscores.html"; // placeholder
+}
+
+function showAbout() {
+  alert("Trivia That'll Trip Ya is a fun, fast-paced trivia game to test your smarts. Choose categories, pick a timer, and beat your own high score!");
+}
