8d019fc2282588090fa0f6156b004543e963f884
soumya15coder <jaiswal788222@sas.edu.sg>
2025-05-09 23:20:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 function startGame() {
-  window.location.href = "game.html"; // you'll create this next
-}
+    window.location.href = "category2.html";
+  }
+  
+  function goToHighScores() {
+    window.location.href = "highscores.html";
+  }
+  
+  function showAbout() {
+    alert("Trivia That'll Trip Ya is a fun, fast-paced trivia game to test your smarts. Choose categories, pick a timer, and beat your own high score!");
+  }
 
-function goToHighScores() {
-  window.location.href = "highscores.html"; // placeholder
-}
-
-function showAbout() {
-  alert("Trivia That'll Trip Ya is a fun, fast-paced trivia game to test your smarts. Choose categories, pick a timer, and beat your own high score!");
-}
+  function handleAnswerSelection(button, isCorrect) {
+    const allButtons = document.querySelectorAll(".option");
+    allButtons.forEach(btn => btn.disabled = true);
+  
+    if (isCorrect) {
+      button.classList.add("correct");
+    } else {
+      button.classList.add("incorrect");
+    }
+  
+    setTimeout(() => {
+      allButtons.forEach(btn => {
+        btn.classList.remove("correct", "incorrect");
+        btn.disabled = false;
+      });
+  
+      loadNextQuestion(); 
+    }, 1000);
+  }
+  
