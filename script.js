d3af0b330fdb53a4e13cb8c3b0776dd2cc3693ab
soumya15coder <jaiswal788222@sas.edu.sg>
2025-05-12 23:34:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,24 @@ function startGame() {
     alert("Trivia That'll Trip Ya is a fun, fast-paced trivia game to test your smarts. Choose categories, pick a timer, and beat your own high score!");
   }
 
-  function handleAnswerSelection(button, isCorrect) {
-    const allButtons = document.querySelectorAll(".option");
-    allButtons.forEach(btn => btn.disabled = true);
+  function handleAnswerSelection(button, isCorrect) {   //takes in button user clicked on + if button is correct
+    const allButtons = document.querySelectorAll(".option"); //stores all the buttons on the page in the NodeList allButtons
+    allButtons.forEach(btn => btn.disabled = true); //disables all buttons right after one's clicked so can’t click multiple buttons
   
     if (isCorrect) {
-      button.classList.add("correct");
+      button.classList.add("correct");  //adds the class correct to style the button green (defined in styles.css)
     } else {
-      button.classList.add("incorrect");
+      button.classList.add("incorrect");  //adds the class incorrect to style the button red (defined in styles.css)
     }
   
-    setTimeout(() => {
+    //setting next question after 1 second
+    setTimeout(() => {      //by default waits for a second timeout before moving on to the next quesiton, showing button animation
       allButtons.forEach(btn => {
-        btn.classList.remove("correct", "incorrect");
-        btn.disabled = false;
+        btn.classList.remove("correct", "incorrect");   //removes any green or red button styling to move on
+        btn.disabled = false;   //enables buttons to be used again
       });
   
-      loadNextQuestion(); 
-    }, 1000);
+      loadNextQuestion(); //LOADS NEXT Q!!
+    }, 1000);  //this is in milliseconds!! so its ending the timeout after one second. (1 sec = 1000 ms)
   }
   
